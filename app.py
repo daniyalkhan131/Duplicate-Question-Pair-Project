90207e42b35fd21a9efdf653549bc2a4e7179a0b
daniyalkhan131 <daniyalkhan131@gmail.com>
2023-12-28 21:54:07 +0530

    added hello world

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-print("hello world")
+import random
+import sys
+#import yaml
+from dvclive import Live
+import mlflow
+
+with Live(save_dvc_exp=True) as live:
+    epochs = 10 #we are tracking for 10 epochs and saving records  for each epoch
+
+    #we can change params.yaml file in dvclive to control things, anf for manipulating .yaml use uaml lib
+
+    #train_params=yaml.safe_load(open('params.yaml'))['train']
+    #epochs=train_params['epochs ']
+    live.log_param ("epochs", epochs)
+    mlflow.log_param ("epochs", epochs)
+    for epoch in range(epochs):
+        live.log_metric("train/accuracy", epoch + random.random())
+        live.log_metric("train/loss", epochs - epoch - random.random())
+        live.log_metric("val/accuracy", epoch + random.random())
+        live.log_metric("val/loss", epochs - epoch - random.random())
+
+        mlflow.log_metric("train/accuracy", epoch + random.random())
+        mlflow.log_metric("train/loss", epochs - epoch - random.random())
+        mlflow.log_metric("val/accuracy", epoch + random.random())
+        mlflow.log_metric("val/loss", epochs - epoch - random.random())
+
+        live.next_step()
